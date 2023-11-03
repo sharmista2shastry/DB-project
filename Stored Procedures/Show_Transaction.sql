94be579506e52fcf952cf7d671d039f2c8c8c098
Sharmista Shastry <sharmistashastry@Sharmistas-Air.lan>
2023-11-03 12:06:17 -0400

    Added Function to retrieve all the successful transactions for a given customer email

---
+++ Stored Procedures/Show_Transaction.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE FUNCTION GET_SUCCESSFUL_TRANSACTIONS_BY_EMAIL(customer_email TEXT)
+RETURNS TABLE (
+    transaction_id INT,
+    transaction_amount REAL,
+    transaction_currency VARCHAR(100),
+    transaction_fraud BOOLEAN,
+    transaction_timestamp TIMESTAMP,
+    merchant_id INT,
+    acquirer_id INT,
+    transaction_type_id INT,
+    decline_reason_id INT,
+    authentication_type_id INT,
+    card_number VARCHAR(30),
+    cardholder_id INT
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        T.TRANSACTION_ID,
+        T.AMOUNT,
+        T.CURRENCY,
+        T.FRAUD,
+        T.TRANSACTION_TIMESTAMP,
+        T.MERCHANT_ID,
+        T.ACQUIRER_ID,
+        T.TRANSACTION_TYPE_ID,
+        T.DECLINE_REASON_ID,
+        T.AUTHENTICATION_TYPE_ID,
+        T.CARD_NUMBER,
+        T.CARDHOLDER_ID
+    FROM
+        TRANSACTIONS T
+    JOIN
+        CARDHOLDER_DETAILS CD ON T.CARDHOLDER_ID = CD.CARDHOLDER_ID
+    WHERE
+        CD.EMAIL = email_id
+        AND T.TRANSACTION_STATUS = TRUE;
+END;
+$$ LANGUAGE plpgsql;
